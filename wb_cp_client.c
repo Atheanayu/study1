3db22101fe035c9a194d68a83d7774dda6613380
Athena <athena@johnMbp.local>
2018-08-15 14:12:06 +0800

    多线程网络拷贝

--- wb_cp_client.c
+++ wb_cp_client.c
@@ -9,12 +9,11 @@
 #define MAX_LINE 20
 #define THR_NUM 5
 static int PORT = 6000;
-//static char msg[THR_NUM][MAX_LINE] = {"AAA","BBB","CCC"};
 typedef struct {
     struct sockaddr_in sin;
     int sfd,order,fd,max;
 }ARG;
-int get_len(char * path){
+int get_len(char * path){/* 获得文件的长度 */
     int fd,len;
     if((fd = open(path,O_RDONLY))==-1){
         perror("fail at function open");
@@ -26,44 +25,35 @@ int get_len(char * path){
     }
     return len;
 }
-void * thfn(void *arg){
+void * thfn(void *arg){/* 线程函数 */
     ARG * p = (ARG*)arg;
     char msg[p->max];
     char command[MAX_LINE];
-    if((p->sfd = socket(AF_INET,SOCK_STREAM,0))==-1){
+    if((p->sfd = socket(AF_INET,SOCK_STREAM,0))==-1){/* 创建socket */
         perror("fail at function socket");
         return (void *)-1;
     }
-    if(connect(p->sfd,(const struct sockaddr *)&(p->sin),sizeof(struct sockaddr))==-1){
+    if(connect(p->sfd,(const struct sockaddr *)&(p->sin),sizeof(struct sockaddr))==-1){/* 发出连接请求 */
         perror("fail at function connect");
         return (void *)-1;
     }
-    if(lseek(p->fd,(p->order)*(p->max),SEEK_SET)==-1){
+    if(lseek(p->fd,(p->order)*(p->max),SEEK_SET)==-1){/* 定位文件指针 */
         perror("fail at function lseek");
         exit(1);
     }
-    sprintf(command,"SUC %d %d",p->max,p->order);
-    if((int)read(p->fd,msg,(size_t)p->max)==-1){
+    sprintf(command,"SUC %d %d",p->max,p->order);/* 写入命令 */
+    if((int)read(p->fd,msg,(size_t)p->max)==-1){/* 从本地文件中读入 */
         perror("fail at function read");
         return (void *)-1;
     }
-    if(write(p->sfd,command,MAX_LINE)==-1){
+    if(write(p->sfd,command,MAX_LINE)==-1){/* 将命令传到server端 */
         perror("fail at function write");
         return (void *)-1;
     }
-    if(write(p->sfd,msg,strlen(msg))==-1){
+    if(write(p->sfd,msg,strlen(msg))==-1){/* 将读入内容传到server端 */
         perror("fail at function write");
         return (void *)-1;
     }
-//    if(read(p->sfd,p->msg2,MAX_LINE)==-1){
-//        perror("fail at function read");
-//        return (void *)-1;
-//    }
-//    if(write(STDOUT_FILENO,p->msg2,strlen(p->msg2))==-1){
-//        perror("fail at function write");
-//        return (void *)-1;
-//    }
-//    fflush(stdout);
     close(p->sfd);
     close(p->fd);
     return (void *)0;
@@ -71,23 +61,24 @@ void * thfn(void *arg){
 int main(){
     ARG arg[THR_NUM];
     pthread_t mtid;
-    int fd;
-    int len = get_len("t1.txt");
-    int max = len/THR_NUM + 1;
-    for(int i = 0;i<THR_NUM;i++)
+    int len = get_len("t1.txt");/* 获得文件长度 */
+    int max = len/THR_NUM + 1;/* 获得一次读入的最大长度 */
+    for(int i = 0;i<THR_NUM;i++)/* 为文件指针排好顺序 */
         arg[i].order = i;
     for(int i = 0;i<THR_NUM;i++){
-        bzero(&(arg[i].sin),sizeof(struct sockaddr_in));
+        bzero(&(arg[i].sin),sizeof(struct sockaddr_in));/* 清空地址结构 */
+        /* 赋值 */
         arg[i].sin.sin_family = AF_INET;
         inet_pton(AF_INET, "127.0.0.1", &((arg[i].sin).sin_addr));
         arg[i].sin.sin_port = htons(PORT);
+
         arg[i].max = max;
-        if((arg[i].fd=open("t1.txt",O_RDONLY))==-1){
+        if((arg[i].fd=open("t1.txt",O_RDONLY))==-1){/* 打开要读的文件 */
             perror("fail at function open");
             exit(1);
         }
         mtid = pthread_self();
-        pthread_create(&mtid,NULL,thfn,&(arg[i]));
+        pthread_create(&mtid,NULL,thfn,&(arg[i]));/* 开线程 */
     }
     sleep(10);
     exit(0);
--- wb_cp_server.c
+++ wb_cp_server.c
@@ -13,81 +13,79 @@ typedef struct{
     int cfd,fd;
     int order,max;
 }ARG;
-void * thfn(void * arg){
+void * thfn(void * arg){/* 线程函数 */
     ARG *p = (ARG*) arg;
     int len=0;
     char *buf=NULL;
     char command[MAX_LINE];
     int n;
-    if(read(p->cfd,command,MAX_LINE)==-1){
+    if(read(p->cfd,command,MAX_LINE)==-1){/* 读取命令 */
         perror("fail at function read1");
         return (void *)-1;
     }
-    for(int i = 4;command[i] !='\0';i++){
+    for(int i = 4;command[i] !='\0';i++){/* 读取命令中的有用信息：一次读取的最大长度和自己是第几个文件指针 */
         if('0'<=command[i]&&command[i]<='9'){
             len*=10;
             len+=(command[i]-'0');
-        }else if(command[i]==' '){
-            buf = (char *)malloc((size_t)len);
-            p->max = len;
-            len = 0;
+        }else if(command[i]==' '){/* 说明到了下一个参数 */
+            buf = (char *)malloc((size_t)len);/* max即为给buf分配的长度 */
+            p->max = len;/* len第一次读的是max，将max赋值 */
+            len = 0;/* 为读下一个参数做准备 */
         }
     }
-    p->order = len;
+    p->order = len;/* 第二次读出的len是order的值 */
 
-    if(lseek(p->fd,(p->order)*(p->max),SEEK_SET)==-1){
+    if(lseek(p->fd,(p->order)*(p->max),SEEK_SET)==-1){/* 为文件指针定位 */
         perror("fail at function lseek");
         return (void *)-1;
     }
-    if((n = (int)read(p->cfd,buf,(size_t)p->max))==-1){
+    if((n = (int)read(p->cfd,buf,(size_t)p->max))==-1){/* 读从client端传入的内容 */
         perror("fail at function read2");
         return (void *)-1;
     }
-    if(write(p->fd,buf,(size_t)n)==-1){
+    if(write(p->fd,buf,(size_t)n)==-1){/* 将读入的内容传到本地的文件中 */
         perror("fail at function write");
         return (void *)-1;
     }
-    close(p->fd);
-    free(buf);
+    close(p->fd);/* 关闭文件描述符 */
+    free(buf);/* free掉malloc的内存 */
     return (void *)0;
 }
 
 int main(){
     struct sockaddr_in sin,cin;
-    int lfd,i,fd;
+    int lfd,i;
     ARG arg[THR_NUM];
     pthread_t mtid;
-    for(i = 0;i<THR_NUM;i++)
-        arg[i].order = i;
-    memset(&(sin),0,sizeof(struct sockaddr_in));
+    memset(&(sin),0,sizeof(struct sockaddr_in));/* 清空地址结构 */
     sin.sin_port = htons(PORT);
     sin.sin_addr.s_addr = INADDR_ANY;
     sin.sin_family = AF_INET;
-    if((lfd = socket(AF_INET,SOCK_STREAM,0))==-1){
+    if((lfd = socket(AF_INET,SOCK_STREAM,0))==-1){/* 创建连接socket */
         perror("fail at function socket");
         exit(1);
     }
-    if(bind(lfd,(const struct sockaddr*)&(sin),sizeof(struct sockaddr))==-1){
+    if(bind(lfd,(const struct sockaddr*)&(sin),sizeof(struct sockaddr))==-1){/* 将连接socket和地址结构绑定 */
         perror("fail at function bind");
         exit(1);
     }
-    if(listen(lfd,10)==-1){
+    if(listen(lfd,10)==-1){/* 监听，此为lfd负责的 */
         perror("fail at function listen");
         exit(1);
     }
     i = 0;
     while(1){
         socklen_t len = sizeof(struct sockaddr);
-        if((arg[i].cfd = accept(lfd,(struct sockaddr*)&(cin),&len))==-1){
+        if((arg[i].cfd = accept(lfd,(struct sockaddr*)&(cin),&len))==-1){/* 如果接收到了来自client的连接请求 */
             perror("fail at function accept");
             exit(1);
         }
-        if((arg[i].fd = open("t2.txt",O_WRONLY))==-1){
+        if((arg[i].fd = open("t2.txt",O_WRONLY))==-1){/* 打开本地文件 */
             perror("fail at function open");
             exit(1);
         }
         mtid = pthread_self();
-        if(pthread_create(&mtid, NULL, thfn, &(arg[i].cfd))==-1) {
+        if(pthread_create(&mtid, NULL, thfn, &(arg[i].cfd))==-1) {/* 创建线程 */
             perror("fail at function pthread_create");
             exit(1);
         }
